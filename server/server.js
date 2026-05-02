bbf38db164bf8515454cb127e710e2f59be49b67
Sudhir Hadge <sudhir.hadge@gmail.com>
2026-05-02 16:44:20 +0530

    (config) : url changed to vite for cors issue

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Fixed methods array
   allowedHeaders: ["Content-Type", "Authorization"],
   preflightContinue: false, // Don't pass the CORS response to the next handler
